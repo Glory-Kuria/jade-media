dbc3df998fb30cd3ba80ab9fe5527f847bdb0d8e
Glory_Kuria <kuriaglory@gmail.com>
2024-07-12 22:51:20 +0300

    made changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-
-import Video from "./videos"; 
-
-function App() {
-  return (
-    <div>
-    <Video/>
-
 import Navbar from "./Navbar"
 import LandingPage from "./LandingPage"
+import Blog from "./Blog";
 import Podcast from "./Podcast";
 import Portfolio from "./Portfolio";
 import "./index.css";
+import Video from "./videos"; 
+
+
+   
+
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <LandingPage/>
+      <Blog/>
       <Podcast/>
       <Portfolio/>
+      <Video/>
      
 
     </div>
