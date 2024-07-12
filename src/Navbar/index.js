4f0e59f161ac2b63f5d551f66a1730dc8e5d3101
Caren Chepngetich <chepngetichkosgeicarenn@gmail.com>
2024-07-12 13:42:04 +0300

    made changes

---
+++ src/Navbar/index.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './index.css';
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div className='navs'>
+      <div className='logo_navbar'>
+      <div className='navbar_logo'>
+      <div>
+   <img className ='logo'src="/Image/Multimedia_Logo-removebg-preview.png" alt="logo"/>
+        </div>
+        <div className='navbar'>
+        <ul>Home</ul>
+        <ul>Blog</ul>
+        <ul>Portfolio</ul>
+        <ul>Podcast</ul>
+        <ul>Products</ul>
+        <ul>Videos</ul>
+        </div>
+        </div>
+         <div className='hamburger' onClick={toggleMenu}>
+         <div className='bar'></div>
+         <div className='bar'></div>
+         <div className='bar'></div>
+        </div> 
+      </div>
+
+      {showMenu && (
+        <div className='menu'>
+          <li>Home</li>
+          <li>Blog</li>
+          <li>Portfolio</li>
+          <li>Podcast</li>
+          <li>Products</li>
+          <li>Videos</li>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
+
