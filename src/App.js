aca63d3c0e97cd0c1fe164c39e1416b9f66a3c87
Glory_Kuria <kuriaglory@gmail.com>
2024-07-12 22:16:33 +0300

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 
 import Video from "./videos"; 
-
-function App() {
-  return (
-    <div>
-    <Video/>
-
 import Navbar from "./Navbar"
 import LandingPage from "./LandingPage"
-import Blog from "./Blog"
 import Podcast from "./Podcast";
-import Portfolio from "./Portfolio";
 import "./index.css";
-
-
-
-
-function App() {
+import Blog from "./Blog";
+function App(){
   return (
     <div className="App">
       <Navbar/>
       <LandingPage/>
       <Blog/>
       <Podcast/>
-      <Portfolio/>
+      <Video/>
      
 
     </div>
   );
-}
+};
 
 export default App;
